4d04c3f33bc5ec325bbad9087408b7f0ca091367
Connor Crossley <cacrossley22@gmail.com>
2018-06-12 12:57:55 -0700

    Added the first question to the guessing game, checked for yes/no responses and tried to catch non yes/no responses

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 'use strict';
 
-
+// Prompt user for name, respond with a greeting
 var userName = prompt('Hello! What is your name?');
-alert('Nice to meet you, ' + userName);
+alert('Nice to meet you, ' + userName + '. Let\'s play a yes or no guessing game!');
 console.log('Username: ' + userName);
+
+// Prompt user and respond based on the answer
+var likesCamping = prompt('Do I like to go camping?').toLowerCase();
+if(likesCamping === 'yes' || likesCamping === 'y'){
+  alert('You\'re right! I LOVE camping');
+} else if (likesCamping === 'no' || likesCamping === 'n'){
+  alert('Sorry, you missed this one. I LOVE camping!');
+} else {
+  alert('Please respond with a simple (Y) Yes or (N) No for the following questions, thanks!');
+}
+console.log('likesCamping: ' + likesCamping);
+
+
+
