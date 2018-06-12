c5a1566738a56fcb35e277cfd4a733629295284d
Connor Crossley <cacrossley22@gmail.com>
2018-06-12 13:43:11 -0700

    Changed some variable manipulation to follow better coding practices. Also did some minor reformatting for readability

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ alert('Nice to meet you, ' + userName + '. Let\'s play a yes or no guessing game
 console.log('Username: ' + userName);
 
 // Prompt user and respond based on the answer
-var likesCamping = prompt('Do I like to go camping?').toLowerCase();
+var likesCamping = prompt('Do I like to go camping?');
+likesCamping = likesCamping.toLowerCase();
+
 if(likesCamping === 'yes' || likesCamping === 'y'){
   alert('You\'re right! I LOVE camping');
 } else if (likesCamping === 'no' || likesCamping === 'n'){
@@ -17,7 +19,9 @@ if(likesCamping === 'yes' || likesCamping === 'y'){
 console.log('likesCamping: ' + likesCamping);
 
 // Prompt user and respond based on the answer
-var hasDog = prompt('Do I have a dog?').toLowerCase();
+var hasDog = prompt('Do I have a dog?');
+hasDog = hasDog.toLowerCase();
+
 if(hasDog === 'yes' || hasDog === 'y'){
   alert('You\'re right, I have a big fluffy Alaskan Malamute named Sledge!');
 } else if (hasDog === 'no' || hasDog === 'n'){
@@ -28,7 +32,9 @@ if(hasDog === 'yes' || hasDog === 'y'){
 console.log('hasDog: ' + hasDog);
 
 // Prompt user and respond based on the answer
-var isFunny = prompt('Do you think I\'m funny?').toLowerCase();
+var isFunny = prompt('Do you think I\'m funny?');
+isFunny = isFunny.toLowerCase();
+
 if(isFunny === 'yes' || isFunny === 'y'){
   alert('Well.... you\'re right, I\'m obviously hilarious!');
 } else if (isFunny === 'no' || isFunny === 'n'){
@@ -39,7 +45,9 @@ if(isFunny === 'yes' || isFunny === 'y'){
 console.log('isFunny: ' + isFunny);
 
 // Prompt user and respond based on the answer
-var isBatman = prompt('Am I Batman?').toLowerCase();
+var isBatman = prompt('Am I Batman?');
+isBatman = isBatman.toLowerCase();
+
 if(isBatman === 'yes' || isBatman === 'y'){
   alert('I\'m BATMAN');
 } else if (isBatman === 'no' || isBatman === 'n'){
@@ -50,7 +58,9 @@ if(isBatman === 'yes' || isBatman === 'y'){
 console.log('isBatman: ' + isBatman);
 
 // Prompt user and respond based on the answer
-var isBilingual = prompt('Am I bilingual?').toLowerCase();
+var isBilingual = prompt('Am I bilingual?');
+isBilingual = isBilingual.toLowerCase();
+
 if(isBilingual === 'yes' || isBilingual === 'y'){
   alert('Well not quite, but I\'d love to be bilingual someday soon!');
 } else if (isBilingual === 'no' || isBilingual === 'n'){
