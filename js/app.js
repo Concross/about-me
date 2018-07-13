535696d7f7ddc326096b7138970897c6fafec319
Connor <cacrossley22@gmail.com>
2018-07-13 11:54:04 -0700

    debug live-server issue

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
-'use strict';
+// 'use strict';
 
-var yesNoQuestionsArray = [];
-var yesNoQuestionsPool = [{question: 'Do I love camping?', answer: true}, {question: 'Do I have a dog?', answer: true}, {question: 'Have I run a half marathon?', answer: false}, {question: 'Am I batman?', answer: true}, {question: 'Am I vegetarian?', answer: false}];
-/***********************************
-*   Yes No Question Constructor    *
-************************************/
+// /***********************************
+// *     DOM Access                   *
+// ************************************/
+// var gameSectionUl = document.getElementById('guessing-game-section');
 
-function YesNoQuestion(question, answer) {
-  this.question = question;
-  this.answer = answer;
-  yesNoQuestionsArray.push(this);
-}
+// /***********************************
+// *     Global Variables             *
+// ************************************/
+// var yesNoQuestionsArray = [];
+// var yesNoQuestionsPool = [{question: 'Do I love camping?', answer: true}, {question: 'Do I have a dog?', answer: true}, {question: 'Have I run a half marathon?', answer: false}, {question: 'Am I batman?', answer: true}, {question: 'Am I vegetarian?', answer: false}];
 
-function runGame() {
-  for (var i = 0; i < yesNoQuestionsPool.length; i++) {
-    new YesNoQuestion(yesNoQuestionsPool[i].question, yesNoQuestionsPool[i].answer);
-  }
-}
+// /***********************************
+// *   Yes No Question Constructor    *
+// ************************************/
+
+// function YesNoQuestion(question, answer) {
+//   this.question = question;
+//   this.answer = answer;
+//   yesNoQuestionsArray.push(this);
+// }
+
+// YesNoQuestion.renderAll = function() {
+//   for (var i = 0; i < yesNoQuestionsArray.length; i++) {
+//     var liEl = document.createElement('li');
+//     var h2El = document.createElement('h2');
+//     console.log(yesNoQuestionsArray[i].question);
+//     h2El.textContent = yesNoQuestionsArray[i].question;
+//     liEl.appendChild(h2El);
+//     gameSectionUl.appendChild(liEl);
+//   }
+// };
+
+// function runGame() {
+//   for (var i = 0; i < yesNoQuestionsPool.length; i++) {
+//     new YesNoQuestion(yesNoQuestionsPool[i].question, yesNoQuestionsPool[i].answer);
+//   }
+//   YesNoQuestion.renderAll();
+// }
+
+// runGame();
