6c73f88db7a60da620042634a8b730e4e020e013
Connor <cacrossley22@gmail.com>
2018-07-13 13:42:36 -0700

    add header and nav to game page

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var gameSectionUl = document.getElementById('guessing-game-section');
 ************************************/
 var userScore = 0;
 var yesNoQuestionsArray = [];
-var yesNoQuestionsPool = [{ question: 'Do I love camping?', answer: 1}, { question: 'Do I have a dog?', answer: 1}, { question: 'Have I run a half marathon?', answer: 0}, { question: 'Am I batman?', answer: 1}, { question: 'Am I vegetarian?', answer: 0}];
+var yesNoQuestionsPool = [{ question: 'Do I love camping?', answer: 1 }, { question: 'Do I have a dog?', answer: 1 }, { question: 'Have I run a half marathon?', answer: 0 }, { question: 'Am I batman?', answer: 1 }, { question: 'Am I vegetarian?', answer: 0 }];
 
 var possibleAnswers = [['no', 'n'], ['yes', 'y']];
 var wrongAnswers = [['yes', 'y'], ['no', 'n']];
@@ -35,9 +35,14 @@ YesNoQuestion.renderAll = function () {
   for (var i = 0; i < yesNoQuestionsArray.length; i++) {
     var liEl = document.createElement('li');
     var h2El = document.createElement('h2');
-    console.log(yesNoQuestionsArray[i].question);
+    var pEl = document.createElement('p');
+
     h2El.textContent = yesNoQuestionsArray[i].question;
     liEl.appendChild(h2El);
+
+    pEl.textContent = 'The correct answer was ' + possibleAnswers[yesNoQuestionsArray[i].answer][0] + ' or ' + possibleAnswers[yesNoQuestionsArray[i].answer][1] + '. You answered ' + yesNoQuestionsArray[i].userAnswer;
+    liEl.appendChild(pEl);
+
     gameSectionUl.appendChild(liEl);
   }
 };
@@ -53,7 +58,7 @@ function askYesNo(questionObj) {
     questionObj.userAnswer = prompt(questionObj.question).toLowerCase();
     console.log('Correct Answer: ' + questionObj.answer);
     console.log('User Answer: ' + questionObj.userAnswer);
-    if (questionObj.userAnswer === possibleAnswers[questionObj.answer][0] || questionObj.userAnswer === possibleAnswers[questionObj.answer][1]){
+    if (questionObj.userAnswer === possibleAnswers[questionObj.answer][0] || questionObj.userAnswer === possibleAnswers[questionObj.answer][1]) {
       alert('Nice! You got it right.');
       updateScore();
       notAnswered = false;
@@ -76,8 +81,8 @@ function runGame() {
   for (var i = 0; i < yesNoQuestionsPool.length; i++) {
     new YesNoQuestion(yesNoQuestionsPool[i].question, yesNoQuestionsPool[i].answer);
   }
-  YesNoQuestion.renderAll();
   YesNoQuestion.askAll();
+  YesNoQuestion.renderAll();
   console.log(userScore);
 }
 
